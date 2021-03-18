27026aa1869dd25dcd89d7ea5476853306fd21a2
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-03-18 16:06:51 +0000

    Maps over added items

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,33 +1,24 @@
 import React, {useState} from "react";
-import ProgressBar from 'react-animated-progress-bar';
-
-
+//import ProgressBar from 'react-animated-progress-bar';
 
 function ButtonComponent() {
 
- const [newInput, setNewInput] = useState("")
+ const [newInput, setNewInput] = useState("");
+ const [itemList, setItemList] = useState([]);
 
-  function handleButtonClick() {
-    return (
-      <ProgressBar
-        width="400px"
-        height="10px"
-        rect
-        fontColor="gray"
-        percentage="100"
-        rectPadding="1px"
-        rectBorderRadius="20px"
-        trackPathColor="transparent"
-        bgColor="#333333"
-        trackBorderColor="grey"
-      />)
-  };
+function handleButtonClick() {
+  let newItem = newInput;
+  setItemList([newItem, ...itemList]);
+  setNewInput("")
+};
 
  return (
  <div>
      <input value = {newInput} onChange={(event) => setNewInput(event.target.value)} placeholder = "What's wrong?"></input>
      
      <button onClick = {handleButtonClick}>Make Everything Okay</button>
+     <ul>{itemList.map((item) => <li>{item}</li>)}</ul>
+     {/* <p>{itemList.map((item) => <li>{itemList[-1]}</li>)}</p> */}
      {console.log(newInput)}
  </div>
  )
@@ -35,3 +26,21 @@ function ButtonComponent() {
 //onClick = {handleButtonClick} 
 
 export default ButtonComponent;
+
+
+
+{/* // function progressBar() {
+  //   return (
+  //     <ProgressBar
+  //       width="400px"
+  //       height="10px"
+  //       rect
+  //       fontColor="gray"
+  //       percentage="100"
+  //       rectPadding="1px"
+  //       rectBorderRadius="20px"
+  //       trackPathColor="transparent"
+  //       bgColor="#333333"
+  //       trackBorderColor="grey"
+  //     />)
+  // }; */}
